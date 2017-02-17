e998d389c78d50317102f259e0b02476851a84b5
Rafael Matias <rafael.matias@mstech.com.br>
2017-02-17 10:30:07 -0200

    Primeiro commit

---
+++ ClipBoard/ClipBoardList.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ClipBoard
+{
+    partial class ClipBoardList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClipBoardList));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Order = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuOpen,
+            this.toolStripMenuClose});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(104, 48);
+            // 
+            // toolStripMenuOpen
+            // 
+            this.toolStripMenuOpen.Name = "toolStripMenuOpen";
+            this.toolStripMenuOpen.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuOpen.Text = "Open";
+            this.toolStripMenuOpen.Click += new System.EventHandler(this.toolStripMenuOpen_Click);
+            // 
+            // toolStripMenuClose
+            // 
+            this.toolStripMenuClose.Name = "toolStripMenuClose";
+            this.toolStripMenuClose.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuClose.Text = "Close";
+            this.toolStripMenuClose.Click += new System.EventHandler(this.toolStripMenuClose_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Order,
+            this.Description,
+            this.Id});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(260, 237);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Order
+            // 
+            this.Order.DataPropertyName = "Order";
+            this.Order.HeaderText = "";
+            this.Order.Name = "Order";
+            this.Order.ReadOnly = true;
+            this.Order.Width = 60;
+            // 
+            // Description
+            // 
+            this.Description.DataPropertyName = "Description";
+            this.Description.HeaderText = "";
+            this.Description.Name = "Description";
+            this.Description.ReadOnly = true;
+            this.Description.Width = 195;
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            // 
+            // ClipBoardList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ClipBoardList";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clipboard";
+            this.Resize += new System.EventHandler(this.Configuration_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuClose;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Order;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+    }
+}
+
---
+++ ClipBoard/ClipBoardList.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ClipBoard
+{
+    public partial class ClipBoardList : Form
+    {
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        IntPtr nextClipboardViewer;
+
+        SQLiteConnection m_dbConnection;
+
+        List<ClipboardFile> list;
+
+        public List<ClipboardFile> ListClipboardFile
+        {
+            get
+            {
+                if (list == null)
+                    list = new List<ClipboardFile>();
+
+                return list;
+            }
+        }
+
+        public ClipBoardList()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
+            ConfigureNotifyIcon();
+            WindowState = FormWindowState.Minimized;
+
+            if (!File.Exists("MyDatabase.sqlite"))
+                SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            OpenConnection();
+
+            if (!TableExists("Registers"))
+            {
+                string sql = "CREATE TABLE Registers (id INT, text VARCHAR(5000))";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+            }
+
+            LoadClipboardList();
+        }
+
+        private void Configuration_Resize(object sender, System.EventArgs e)
+        {
+            ConfigureNotifyIcon();
+        }
+
+        private void OpenConnection()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        private void ConfigureNotifyIcon()
+        {
+            //Exibe o notifyIcon ao lado do relógio
+            notifyIcon.Visible = true;
+
+            //Nome exibido no notifyIcon
+            notifyIcon.Text = this.Text;
+
+            //// Aqui escondemos o form da taskbar...
+            ShowInTaskbar = false;
+
+            Hide();
+        }
+
+        private void InsertText(string text)
+        {
+            VerifyIfExistsText(text);
+            var id = IdIncrement();
+            string sql = "insert into Registers (id, text) values (" + id.ToString() + ", '" + text + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        private int IdIncrement()
+        {
+            string sql = "SELECT MAX(id) FROM Registers";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            var newId = Convert.ToInt32(command.ExecuteScalar()) + 1;
+            return newId;
+        }
+
+        private void VerifyIfExistsText(string text)
+        {
+            string sql = "SELECT id FROM Registers WHERE text='"+ text +"'";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            var id = Convert.ToInt32(command.ExecuteScalar());
+
+            if (id > 0)
+                DeleteRegister(id);
+        }
+
+        private void DeleteRegister(int id)
+        {
+            string sql = "DELETE FROM Registers WHERE id=" + id.ToString() + "";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        public bool TableExists(string tableName)
+        {
+            Debug.Assert(m_dbConnection != null);
+            Debug.Assert(!string.IsNullOrWhiteSpace(tableName));
+
+            var cmd = m_dbConnection.CreateCommand();
+            cmd.CommandText = @"SELECT COUNT(*) FROM sqlite_master WHERE name=@TableName";
+            var p1 = cmd.CreateParameter();
+            p1.DbType = DbType.String;
+            p1.ParameterName = "TableName";
+            p1.Value = tableName;
+            cmd.Parameters.Add(p1);
+
+            var result = cmd.ExecuteScalar();
+            return ((long)result) == 1;
+        }
+
+        private void LoadClipboardList()
+        {
+            string sql = "SELECT * FROM Registers";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            int contador = 1;
+
+            while (reader.Read())
+            {
+                var register = new ClipboardFile();
+                register.Id = (int)reader["id"];
+                register.Description = (string)reader["text"];
+                register.Order = contador;
+                contador++;
+                ListClipboardFile.Add(register);
+            }
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ScrollBars = ScrollBars.Vertical;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dataGridView1.DataSource = ListClipboardFile;
+        }
+
+        private void toolStripMenuOpen_Click(object sender, System.EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            Show();
+        }
+
+        private void toolStripMenuClose_Click(object sender, System.EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // defined in winuser.h
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    DisplayClipboardData();
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        void DisplayClipboardData()
+        {
+            try
+            {
+                IDataObject iData = new DataObject();
+                iData = Clipboard.GetDataObject();
+
+                if (m_dbConnection == null)
+                    OpenConnection();
+
+                if (iData.GetDataPresent(DataFormats.Text))
+                    InsertText((string)iData.GetData(DataFormats.Text));
+
+                //if (iData.GetDataPresent(DataFormats.Rtf))
+                //    richTextBox1.Rtf = (string)iData.GetData(DataFormats.Rtf);
+                //else if (iData.GetDataPresent(DataFormats.Text))
+                //    richTextBox1.Text = (string)iData.GetData(DataFormats.Text);
+                //else
+                //    richTextBox1.Text = "[Clipboard data is not RTF or ASCII Text]";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
